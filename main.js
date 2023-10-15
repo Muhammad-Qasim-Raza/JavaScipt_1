2d3206cb69c3b7912eaba642273cfe62cd4aff1d
Muhammad Qasim Raza <m.qasimrazamanzoorahmed.3@gmail.com>
2023-10-15 13:52:33 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// AlertS CHAPTER #1
+
 alert("Hello, and welcome to my website!");
 
 alert("Error! Please enter a valid password.");
@@ -11,3 +13,39 @@ alert("Happy Coding!\nPrevent this page from creating additional dialogs.")
 alert("Hello... I can run JS through my web browser's console")
 
 alert("Hello... I can run JS through my web browser's console")
+
+
+// VARIABLES FOR STRINGS CHAPTER #2
+
+var username;
+
+var myName = "Muhammad Qasim Raza";
+
+var message;
+message = "Hello World";
+alert("Message: " + message);
+
+var studentName = "Muhammad Qasim Raza";
+var age = 21;
+var address = "Karachi";
+var phoneNumber = "0347 2584939";
+
+var bioData = "Student Name: " + studentName + "\n" +
+    "Age: " + age + " years\n" +
+    "Address: " + address + "\n" +
+    "Phone Number: " + phoneNumber;
+
+alert(bioData);
+
+alert("Pizza\nPizz\nPiz\nPi\nP");
+
+var email = "qasim@example.com";
+var message = "My email address is: " + email;
+alert(message);
+
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+document.write("Yah! I can write HTML content through JavaScript");
+
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
