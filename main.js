8f64b1d7399df3ab7bf77176f91f3b6240333a99
Muhammad Qasim Raza <115484433+Muhammad-Qasim-Raza@users.noreply.github.com>
2023-10-15 13:27:38 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+alert("Hello, and welcome to my website!");
+
+alert("Error! Please enter a valid password.");
+
+alert("Welcome to JS Land...\nHappy Coding!");
+
+alert("Welcome to JS Land...")
+
+alert("Happy Coding!\nPrevent this page from creating additional dialogs.")
+
+alert("Hello... I can run JS through my web browser's console")
+
+alert("Hello... I can run JS through my web browser's console")
